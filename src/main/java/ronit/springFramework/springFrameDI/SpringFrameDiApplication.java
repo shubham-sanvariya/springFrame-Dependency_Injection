d1cf784c511b5db55d2edd6b499617e40626f308
shubham-sanvariya <shubhamsanvariyait.com>
2022-12-19 12:52:50 +0530

    Initial commit

---
+++ src/main/java/ronit/springFramework/springFrameDI/SpringFrameDiApplication.java
@@ -0,0 +1,13 @@
+package ronit.springFramework.springFrameDI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ronit/springFramework/springFrameDI/SpringFrameDiApplicationTests.java
@@ -0,0 +1,13 @@
+package ronit.springFramework.springFrameDI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

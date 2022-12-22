e2e62fa943f074edf3e8b2ffbafaafc7dc1c8e35
shubham-sanvariya <shubhamsanvariyait.com>
2022-12-22 14:55:23 +0530

    using profiler to load the active bean

---
+++ src/main/java/ronit/springFramework/springFrameDI/controllers/I18nController.java
@@ -0,0 +1,18 @@
+package ronit.springFramework.springFrameDI.controllers;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import ronit.springFramework.springFrameDI.services.GreetingSerices;
+
+@Controller
+public class I18nController {
+    private final GreetingSerices greetingSerices;
+
+    public I18nController(@Qualifier("i18nService") GreetingSerices greetingSerices) {
+        this.greetingSerices = greetingSerices;
+    }
+
+    public String sayHello() {
+        return greetingSerices.sayGreeting();
+    }
+}

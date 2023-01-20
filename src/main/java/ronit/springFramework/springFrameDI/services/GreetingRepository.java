8391e330dc3c9082ab6ba0347d6f836e15ef32b7
shubham-sanvariya <shubhamsanvariyait.com>
2023-01-20 23:24:28 +0530

    Services

---
+++ src/main/java/ronit/springFramework/springFrameDI/services/GreetingRepository.java
@@ -0,0 +1,13 @@
+package ronit.springFramework.springFrameDI.services;
+
+/**
+ * Created by jt on 5/24/17.
+ */
+public interface GreetingRepository {
+
+    String getEnglishGreeting();
+
+    String getSpanishGreeting();
+
+    String getGermanGreeting();
+}
---
+++ src/main/java/ronit/springFramework/springFrameDI/services/GreetingRepositoryImpl.java
@@ -0,0 +1,28 @@
+package ronit.springFramework.springFrameDI.services;
+
+import org.springframework.stereotype.Component;
+
+
+/**
+ * Created by jt on 5/24/17.
+ */
+@Component
+public class GreetingRepositoryImpl implements GreetingRepository {
+
+    @Override
+    public String getEnglishGreeting() {
+        return "Hello - Primary Greeting service";
+    }
+
+    @Override
+    public String getSpanishGreeting() {
+        return "Servicio de Saludo Primario";
+    }
+
+    @Override
+    public String getGermanGreeting() {
+
+        return "Primärer Grußdienst";
+
+    }
+}

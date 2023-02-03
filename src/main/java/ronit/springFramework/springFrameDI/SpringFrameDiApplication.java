a473c2720a81bd76418769dcea9e894746694ddd
shubham-sanvariya <shubhamsanvariyait.com>
2023-02-03 15:45:35 +0530

    Spring Environment Properties

--- src/main/java/ronit/springFramework/springFrameDI/SpringFrameDiApplication.java
+++ src/main/java/ronit/springFramework/springFrameDI/SpringFrameDiApplication.java
@@ -1,10 +1,12 @@
 package ronit.springFramework.springFrameDI;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
+import org.springframework.core.env.Environment;
 import ronit.springFramework.springFrameDI.controllers.*;
 import ronit.springFramework.springFrameDI.exampleBeans.FakeDataSource;
 
--- src/main/java/ronit/springFramework/springFrameDI/config/PropertyConfig.java
+++ src/main/java/ronit/springFramework/springFrameDI/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package ronit.springFramework.springFrameDI.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 import ronit.springFramework.springFrameDI.exampleBeans.FakeDataSource;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${shubh.username}")
     String user;
@@ -23,7 +28,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
